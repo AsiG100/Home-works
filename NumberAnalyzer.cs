b29c8b7ae3ecf72e8011ed3185a5a0eed6ad6565
asaf golob <asi.g100@gmail.com>
2017-08-13 14:50:39 +0300

    c17_ex01_1

---
+++ NumberAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿namespace c17_Ex01_1
+{
+    class NumberAnalyzer
+    {
+        public static string[] convertToBin(string[] decimals)
+        {
+            string[] bins = { "", "", "", "" };
+
+            for(int i = 0; i < decimals.Length; i++)
+            {
+                int num = int.Parse(decimals[i]);
+                while(num>0)
+                {
+                    if(num%2==0)
+                    {
+                        bins[i] += '0';
+                    }
+                    else
+                    {
+                        bins[i] += '1';
+                    }
+                    num /= 2;
+                }
+            }
+
+            return bins;
+        }
+    }
+}
