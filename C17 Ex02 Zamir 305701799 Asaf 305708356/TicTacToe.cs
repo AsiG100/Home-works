1a38ef91f3d479940c9af2a58b76b3ea649a02f6
asaf golob <asi.g100@gmail.com>
2017-08-24 14:02:09 +0300

    Add files via upload

--- C17 Ex02 Zamir 305701799 Asaf 305708356/TicTacToe.cs
+++ C17 Ex02 Zamir 305701799 Asaf 305708356/TicTacToe.cs
@@ -8,25 +8,27 @@ namespace C17_Ex02_Zamir_305701799_Asaf_305708356
 {
     class TicTacToe
     {
-        public static void drawTable(int size,char [,] mat)
+        public static void drawTable(char[,] io_mat)
         {
+            int edgeSize = io_mat.GetLength(0);
+
             ///The upper area
-            for(int i=0;i<size;i++)
+            for(int i=0;i<edgeSize;i++)
             {
                 Console.Write("   {0}", i + 1);
             }
             Console.WriteLine();
             ///The table
-            for(int i=0;i<size;i++)
+            for(int i=0;i<edgeSize;i++)
             {
                 Console.Write(i + 1);
-                for(int j=0; j<size;j++)
+                for(int j=0; j<edgeSize;j++)
                 {
-                    Console.Write("|{0}  ",mat[i,j]);
+                    Console.Write("|{0}  ",io_mat[i,j]);
                 }
                 Console.WriteLine("|");
                 Console.Write(" ");
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < edgeSize; j++)
                 {
                     Console.Write("====");
                 }
@@ -44,24 +46,40 @@ namespace C17_Ex02_Zamir_305701799_Asaf_305708356
                         ==================");
             Console.WriteLine("\n");
         }
-        public static void initTable(char[,] table)
+        public static void initTable(char[,] io_table)
         {
-            int length = table.GetLength(0);
+            int length = io_table.GetLength(0);
             for (int i = 0; i < length; i++)
             {
                 for (int j = 0; j < length; j++)
                 {
-                    table[i, j] = ' ';
+                    io_table[i, j] = ' ';
                 }
             }
         }
         // TODO: singleplayer game
-        public static void singlePlayer(char[,] table)
+        public static void singlePlayer(char[,] i_table)
         {
+            bool gameRunning = true;
+            int row, col;
 
+            Console.Clear(); ///todo: replace it with the clear from the dll
+            Console.WriteLine("The single player game begins, follow the orders in every level\n" +
+                              "to quit press 'Q' at any point, press any key to continue");
+            Console.Read();
+
+            while (gameRunning)
+            {
+                Console.Clear();
+                drawTable(i_table);
+                Console.WriteLine("Please enter the row and the col numbers to place your ");
+                row = Console.Read();
+                col = Console.Read();
+
+            }
         }
         //TODO: multiplayer game
-        public static void multiPlayer(char[,] table)
+        public static void multiPlayer(char[,] i_table)
         {
 
         }
