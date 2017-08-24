ef9356c2d17980a45177792a8db3c301d83f1bcc
asaf golob <asi.g100@gmail.com>
2017-08-24 11:53:29 +0300

    Add files via upload

---
+++ C17 Ex02 Zamir 305701799 Asaf 305708356/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C17_Ex02_Zamir_305701799_Asaf_305708356
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loopRunner = true;
+            do
+            {
+                TicTacToe.displayWelcome();///Display welcome
+                Console.WriteLine("Please insert a number to determine the size of the table(or 0 to exit)");
+                try ///ask for the size of the table
+                {
+                    string userSize = Console.ReadLine();
+                    int tableSize;
+                    bool convertionSucceded = int.TryParse(userSize, out tableSize);
+
+                    if (!convertionSucceded)
+                    {
+                        throw new InvalidOperationException("Please enter numbers only");
+                    } 
+                    else if (tableSize < 3 || tableSize > 9) throw new InvalidOperationException("You inserted an invalid number, try again!");
+
+                    char[,] valuesInTable = new char[tableSize, tableSize];///A matrix for the game values
+                    TicTacToe.initTable(valuesInTable);///initiate matrix with white spaces
+                    Console.Clear();
+                    
+                    ///single player or multi player
+                    Console.WriteLine("Would you like to play against another player (s) or against the computer (m)?");
+                    string userDecision = Console.ReadLine();
+                    if (userDecision == "s")
+                    {
+                        TicTacToe.singlePlayer(valuesInTable);
+                    }
+                    else if (userDecision == "m")
+                    {
+                        TicTacToe.multiPlayer(valuesInTable);
+                    }
+                    else
+                    {
+                        throw new Exception("You should only choose s or m, try again");
+                    }
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+
+                Console.Read();
+                Console.Clear();
+            } while(loopRunner);
+        }
+    }
+}
+
+
+
---
+++ C17 Ex02 Zamir 305701799 Asaf 305708356/TicTacToe.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C17_Ex02_Zamir_305701799_Asaf_305708356
+{
+    class TicTacToe
+    {
+        public static void drawTable(int size,char [,] mat)
+        {
+            ///The upper area
+            for(int i=0;i<size;i++)
+            {
+                Console.Write("   {0}", i + 1);
+            }
+            Console.WriteLine();
+            ///The table
+            for(int i=0;i<size;i++)
+            {
+                Console.Write(i + 1);
+                for(int j=0; j<size;j++)
+                {
+                    Console.Write("|{0}  ",mat[i,j]);
+                }
+                Console.WriteLine("|");
+                Console.Write(" ");
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("====");
+                }
+                Console.WriteLine("=");
+            }
+        }
+
+        public static void displayWelcome()
+        {
+            Console.Write(@"
+                        ==================
+                        | Welcome to the |
+                        |  up side down  |
+                        |  Tic Tac Toe   |
+                        ==================");
+            Console.WriteLine("\n");
+        }
+        public static void initTable(char[,] table)
+        {
+            int length = table.GetLength(0);
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    table[i, j] = ' ';
+                }
+            }
+        }
+        // TODO: singleplayer game
+        public static void singlePlayer(char[,] table)
+        {
+
+        }
+        //TODO: multiplayer game
+        public static void multiPlayer(char[,] table)
+        {
+
+        }
+  
+    }
+}
