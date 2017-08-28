2cbe260cb4f86956f12e7bfb03bccec5929005ed
asaf golob <asi.g100@gmail.com>
2017-08-28 19:15:59 +0300

    Add files via upload

--- C17 Ex02 Zamir 305701799 Asaf 305708356/Program.cs
+++ C17 Ex02 Zamir 305701799 Asaf 305708356/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Ex02.ConsoleUtils;
 
 namespace C17_Ex02
 {
@@ -7,13 +6,9 @@ namespace C17_Ex02
     {
         public static void Main(String[] args)
         {
-            const bool v_gameOn = true;
-            while (v_gameOn)
-            {
-                Screen.Clear();
-                TicTacToeUserInterface.displayWelcome(); //Displays welcome to game
-                TicTacToeUserInterface.displayMenu(); //Displays game menu and start the game
-            }
+            TicTacToeUserInterface.displayWelcome(); //Displays welcome to game
+            TicTacToeUserInterface.displayMenu(); //Displays game menu and start the game
+            TicTacToeUserInterface.optionSelect();
         }
     }
 }
